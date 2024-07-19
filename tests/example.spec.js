8c5e796f597027a9013dade8ac12fc49f69a59f0
QAzando <qazando@gmail.com>
2024-07-18 21:55:04 -0300

    First commit

---
+++ tests/example.spec.js
@@ -0,0 +1,18 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Cenário login com sucesso @login @smoke', async ({ page }) => {
+  await page.goto('/login');
+
+  await page.locator('#user').fill('qazando@gmail.com');
+  await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
+  await expect(page.getByRole('heading', { name: 'Login realizado' })).toBeVisible();
+});
+
+test('Validar E-mail Invalido', async ({ page }) => {
+  await page.goto('/login');
+
+  await page.getByRole('button', { name: 'login' }).click();
+  await expect(page.getByText('E-mail inválido')).toBeVisible();
+});
